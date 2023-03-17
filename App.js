a6408f07b4b7d5fc9496db0da5f11124ddd81cc1
SoundhariyaSi <128037023+SoundhariyaSi@users.noreply.github.com>
2023-03-17 15:48:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import World from './Components/World';
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <World/>
+    </div>
+  );
+}
+
+export default App;
